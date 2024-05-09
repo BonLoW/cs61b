decb4888c9c27c6cc565b695ab0d6637f38c42c5
BonLoW <fjk491032423@gmail.com>
2024-05-09 20:39:16 +0800

    2024-5-9

--- hw0b/src/IntList.java
+++
@@ -1,8 +0,0 @@
-public class IntList {
-    public int first;
-    public IntList rest;
-    public IntList(int first, IntList rest) {
-        this.first = first;
-        this.rest = rest;
-    }
-}
